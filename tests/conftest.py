b4e1fd331e399720feee3a05c09ed2624f619a0f
Шепель Ярослав Валерійович <160532292+yaroslavshepel@users.noreply.github.com>
2026-05-21 18:36:38 +0300

    add project files

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
+    
 
 @pytest.fixture
 def driver():
